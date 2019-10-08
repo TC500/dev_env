a5cbe3b60b2c04055fa38afc4925ee924224f363
TC500 <maxiaowei_main@qq.com>
2019-10-08 11:36:51 +0800

    Update .my_zsh_plugin.zsh
    add plugins/colored-man-pages

--- .my_zsh_plugins.zsh
+++ .my_zsh_plugins.zsh
@@ -1,24 +1,17 @@
+if [[ ! -d ~/.zplug ]]; then
+    curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+fi
 source ~/.zplug/init.zsh
-zplug "plugins/git",   from:oh-my-zsh
-zplug "plugins/vi-mode",   from:oh-my-zsh
+zplug "plugins/git", from:oh-my-zsh
+zplug "plugins/vi-mode", from:oh-my-zsh
+zplug "plugins/colored-man-pages", from:oh-my-zsh
+zplug "lib/theme-and-appearance", from:oh-my-zsh
+zplug "themes/ys", from:oh-my-zsh, as:theme
 zplug "rupa/z", use:z.sh
 zplug "zsh-users/zsh-autosuggestions"
 zplug "zsh-users/zsh-completions"
-zplug "zdharma/fast-syntax-highlighting"
-(( ${+aliases[z]} )) && unalias z
-z() {
-  if [[ -z "$*" ]]; then
-    cd "$(_z -l 2>&1 | fzf +s --tac | sed 's/^[0-9,.]* *//')"
-  else
-    _last_z_args="$@"
-    _z "$@"
-  fi
-}
-zz() {
-  cd "$(_z -l 2>&1 | sed 's/^[0-9,.]* *//' | fzf -q "$_last_z_args")"
-}
-alias j=zz
-alias jj=z
+zplug "zdharma/fast-syntax-highlighting", defer:2
+
 # Install plugins if there are plugins that have not been installed
 if ! zplug check --verbose; then
     printf "Install zplug plugins? [y/N]: "
@@ -26,5 +19,9 @@ if ! zplug check --verbose; then
         echo; zplug install
     fi
 fi
-zplug load --verbose
+zplug load # --verbose
 bindkey '^ ' autosuggest-accept
+(( ${+aliases[z]} )) && unalias z
+z() {
+  cd "$(_z -l 2>&1 | fzf --height 40% --nth 2.. --reverse --inline-info +s --tac --query "${*##-* }" | sed 's/^[0-9,.]* *//')"
+}
